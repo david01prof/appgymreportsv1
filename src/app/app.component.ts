809b3279f7eca3816ac8ff1f3d0e242ab8e09a27
david01prof <77610326+david01prof@users.noreply.github.com>
2024-10-15 01:03:56 +0200

    speedDial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-
+import { SpeedDialComponent } from './components/speed-dial/speed-dial.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ToolbarComponent],
+  imports: [RouterOutlet,ToolbarComponent,SpeedDialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
 }
---
+++ src/app/components/speed-dial/speed-dial.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { SpeedDialModule } from 'primeng/speeddial';
+
+@Component({
+  selector: 'app-speed-dial',
+  standalone: true,
+  imports: [SpeedDialModule],
+  template: `
+    <p-speedDial
+      [model]="leftTooltipItems"
+      className="speeddial-right"
+      direction="up"
+    />
+  `,
+  styles: `
+    :host ::ng-deep .speeddial-left {
+    left: 0;
+    bottom: 0;
+    }
+
+    :host ::ng-deep .speeddial-right {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+    }
+
+    :host ::ng-deep .speeddial-right button{
+        height: 40px;
+        width: 40px;
+        background-color: rgb(57, 140, 202);
+        border: none;
+    }
+
+    :host ::ng-deep .speeddial-right ul li a{
+        height: 40px;
+        width: 40px;
+    }
+  `,
+})
+export class SpeedDialComponent {
+  leftTooltipItems: MenuItem[] = [];
+
+  constructor(private route: Router) {}
+
+  ngOnInit() {
+    this.leftTooltipItems = [
+      {
+        tooltipOptions: {
+          tooltipLabel: 'Logout',
+          tooltipPosition: 'left',
+        },
+        icon: 'pi pi-sign-out',
+        command: () => {
+          this.route.navigate(['/login']);
+        },
+      },
+      {
+        tooltipOptions: {
+          tooltipLabel: 'Ajustes',
+          tooltipPosition: 'left',
+        },
+        icon: 'pi pi-cog',
+        command: () => {},
+      },
+      {
+        tooltipOptions: {
+          tooltipLabel: 'Usuario',
+          tooltipPosition: 'left',
+        },
+        icon: 'pi pi-user',
+        command: () => {},
+      },
+    ];
+  }
+}
