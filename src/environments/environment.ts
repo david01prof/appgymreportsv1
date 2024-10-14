6a66c02d9a3da6f1b34e29ab1191d3728b379016
david01prof <77610326+david01prof@users.noreply.github.com>
2024-10-14 13:52:04 +0200

    configuracion inicial

---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    firebase: {"projectId":"gymreportv1","appId":"1:335923470717:web:02f8e30c20d83c0ed52c14","storageBucket":"gymreportv1.appspot.com","apiKey":"AIzaSyDYyzCRKSRA8dmzF21V8ajUGp4UbKcALUM","authDomain":"gymreportv1.firebaseapp.com","messagingSenderId":"335923470717"}
+};
