0eedaad0302b40cdb8c394ac3c46518021f4c8ba
david01prof <77610326+david01prof@users.noreply.github.com>
2024-10-15 00:57:53 +0200

    dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+
+const PRIME_MODULES = [DialogModule, ButtonModule, InputTextModule];
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [PRIME_MODULES,CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  ngAfterViewInit(): void {
+    // this.visible = true;
+    console.log(this.visible);
+    this.currentUrl = this.route.url;
+    console.log(this.currentUrl);
+  }
 
+  currentUrl: string = '';
+
+  constructor(private route: Router) {}
+
+  public date = new Date();
+  public objetiveWeight: number = 78;
+
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+  }
 }
