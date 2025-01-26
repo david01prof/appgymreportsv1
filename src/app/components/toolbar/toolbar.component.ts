8792f5085f747fe41e4a3bad5b8e13cf7b9aa76c
david01prof <77610326+david01prof@users.noreply.github.com>
2025-01-26 23:42:08 +0100

    toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -28,6 +28,7 @@ import { SidebarModule } from 'primeng/sidebar';
       </div>
 
       <p-accordion class="w-full">
+        <!-- Routines -->
         <p-accordionTab>
             <ng-template pTemplate="header">
               <div class="flex justify-content-start align-items-center">
@@ -37,10 +38,11 @@ import { SidebarModule } from 'primeng/sidebar';
                 <h3 class="font-lighter text-xl"  routerLink="/routines">Rutinas</h3>
               </div>
             </ng-template>
-            <p class="m-0 pl-7">  <a class="pi pi-plus mr-1"></a> Nueva rutina </p>
+            <p class="m-0 pl-7">  <a class="pi pi-plus mr-1 text-sm"></a> Nueva rutina </p>
         </p-accordionTab>
       </p-accordion>
 
+      <!-- Reports -->
       <p-accordion class="w-full">
         <p-accordionTab>
             <ng-template pTemplate="header">
@@ -51,11 +53,11 @@ import { SidebarModule } from 'primeng/sidebar';
                 <h3 class="font-lighter text-xl"  routerLink="/reports">Reportes</h3>
               </div>
             </ng-template>
-            <p class="m-0 pl-7">  <a class="pi pi-plus mr-1"></a> Nuevo reporte </p>
+            <p class="m-0 pl-7">  <a class="pi pi-plus mr-1 text-sm"></a> Nuevo reporte </p>
         </p-accordionTab>
       </p-accordion>
 
-      <!-- Home -->
+      <!-- Ajustes -->
       <div class="flex justify-content-start align-items-center pl-3">
         <div class="circle backgroundProfile flex justify-content-center align-items-center mr-3">
           <a class="pi pi-cog alignProfile"></a>
@@ -64,7 +66,7 @@ import { SidebarModule } from 'primeng/sidebar';
       </div>
 
 
-      <!-- Home -->
+      <!-- legal terms -->
       <div class="flex justify-content-start align-items-center pl-3">
         <div class="circle backgroundTerms flex justify-content-center align-items-center mr-3">
           <a class="fa-solid fa-file-pen alignTerms"></a>
@@ -72,76 +74,41 @@ import { SidebarModule } from 'primeng/sidebar';
         <h3 class="font-lighter text-xl"  routerLink="/politic-terms">Política de privacidad y cookies</h3>
       </div>
 
-      <!-- Home -->
+      <!-- Logout -->
       <div class="flex justify-content-start align-items-center pl-3">
         <div class="circle backgroundLogout flex justify-content-center align-items-center mr-3">
           <a class="pi pi-sign-out"></a>
         </div>  
         <h3 class="font-lighter text-xl" (click)=" _authStateSvc.logout()">Cerrar sesión</h3>
       </div>
+
+      <!-- Social media -->
+      <div class="flex justify-content-center gap-4 mt-3">
+      <div class="circle backgroundIntagram flex justify-content-center align-items-center mr-3">
+          <a class="pi pi-instagram"></a>
+        </div>  
+        
+        <div class="flex flex-column text-center">
+        <span class="font-bold text-lg">MiFitTracker</span>
+        <small class="text-sm">Beta</small>
+        </div>
+        <div class="circle backgroundLinkedin flex justify-content-center align-items-center mr-3">
+          <a class="pi pi-linkedin"></a>
+        </div>  
+      </div>
     </p-sidebar>
 
+    <!-- Button Menu -->
     <div class="flex align-items-center gap-2">
-
-          <div class="card flex justify-content-center menuCss">
-            <p-button [link]="true" [text]="true" [raised]="true" [rounded]="true" severity="secondary" (onClick)="sidebarVisible4 = true">
-              <i class="pi pi-list"></i>
-            </p-button>
-          </div>
-        </div>
+      <div class="card flex justify-content-center menuCss">
+        <p-button [link]="true" [text]="true" [raised]="true" [rounded]="true" severity="secondary" (onClick)="sidebarVisible4 = true" icon="pi pi-list"/>
+      </div>
+  </div>
   `,
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
   public sidebarVisible4 = false;
-  public items: MenuItem[] | undefined;
-  public itemsProfile: MenuItem[] | undefined;
 
   public readonly _authStateSvc = inject(AuthStateService);
-  public readonly _globalSvc = inject(GlobalService);
-
-  async ngOnInit() {
-    this.items = [
-      {
-        label: 'Home',
-        icon: 'pi pi-home',
-        route: 'dashboard',
-      },
-      {
-        label: 'Reportes',
-        icon: 'pi pi-star',
-        route: 'reports',
-      },
-      {
-        label: 'Rutinas',
-        icon: 'pi pi-search',
-        route: 'routines',
-      },
-    ];
-
-    this.itemsProfile = [
-      {
-        items: [
-          {
-            label: 'Perfil',
-            icon: 'pi pi-cog',
-            routerLink: ['/profile'],
-          },
-          // {
-          //   label: 'Terminos y condiciones',
-          //   icon: 'pi pi-cog',
-          //   routerLink: ['/politic-terms'],
-          // },
-          {
-            label: 'Salir',
-            icon: 'pi pi-sign-out',
-            command: () => {
-              this._authStateSvc.logout();
-            },
-          },
-        ],
-        label: 'MiFitTracker  BETA',
-      },
-    ];
-  }
 }
